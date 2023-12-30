a81a0c7c8b6af455bab53137a50a7bb03e99f04d
sungsu9022 <sungsu9022@naver.com>
2023-12-30 20:30:26 +0900

    IDE에서 너무 느려서 수동 jar 포함하는 방식 제외

--- elasticsearch/build.gradle.kts
+++ elasticsearch/build.gradle.kts
@@ -22,9 +22,10 @@ dependencies {
     // es 저수준 rest client
     implementation("org.elasticsearch.client:elasticsearch-rest-client:$esVersion")
     // es 고수준 rest client
-    // implementation("org.elasticsearch.client:elasticsearch-rest-high-level-client:$esHighLevelClientVersion")
-    implementation(fileTree(mapOf("dir" to "manual-build", "includes" to listOf("*.jar"))))
-    implementation("org.elasticsearch:elasticsearch:$esVersion")
+     implementation("org.elasticsearch.client:elasticsearch-rest-high-level-client:$esHighLevelClientVersion")
+    // 수동 jar 설치 후 적용하는 방식
+//    implementation(fileTree(mapOf("dir" to "manual-build", "includes" to listOf("*.jar"))))
+//    implementation("org.elasticsearch:elasticsearch:$esVersion")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
